12fcb60634f8f09bf54407223caea7f0970b45da
Roachy <alex.walton@prettylittlething.com>
2016-12-25 18:17:18 +0000

    Setting up the base for our application

---
+++ public/application/views/header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.walton
+ * Date: 25/12/2016
+ * Time: 18:13
+ */
+
+$this->load->view('head');
+$this->load->view('navbar');
---
+++ public/application/views/navbar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex.walton
+ * Date: 25/12/2016
+ * Time: 18:12
+ */
+?>
+<ul class="nav nav-tabs">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul>
